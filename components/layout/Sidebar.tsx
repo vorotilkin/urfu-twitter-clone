b5fd827b1946c1dd6b498a69c6cc9036f92555ea
vorotilkin <96014794+vorotilkin@users.noreply.github.com>
2024-11-12 23:56:13 +0500

    draft

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,50 @@
+import {BsBellFill, BsHouseFill} from "react-icons/bs";
+import {FaUser} from "react-icons/fa";
+import SidebarLogo from "@/components/layout/SidebarLogo";
+import SidebarItem from "@/components/layout/SidebarItem";
+import {BiLogOut} from "react-icons/bi";
+import SidebarTweetButton from "@/components/layout/SidebarTweetButton";
+
+
+const Sidebar = () => {
+    const items = [
+        {
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notifications',
+            href: '/notifications',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/users/123',
+            icon: FaUser
+        }
+    ]
+
+    return (
+        <div className='col-span-1 h-full pr-4 md:pr-6'>
+            <div className='flex flex-col items-end'>
+                <div className='space-y-2 lg:w-[230px]'>
+                    <SidebarLogo/>
+                    {items.map((item) => (
+                        <SidebarItem
+                            key={item.href}
+                            href={item.href}
+                            label={item.label}
+                            icon={item.icon}
+                        />
+                    ))}
+                    <SidebarItem onClick={() => {
+                    }} icon={BiLogOut} label="Logout"/>
+                    <SidebarTweetButton/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
